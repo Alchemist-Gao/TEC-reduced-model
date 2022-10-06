8d35af347da7c4ce09bc0e7a16cbf9cbfb71243e
alchemistgao <614108603@qq.com>
2022-10-06 19:46:40 +0800

    mytest

--- scripts/mytest.py
+++ scripts/mytest.py
@@ -20,8 +20,63 @@ from scipy.interpolate import interp1d
 
 pybamm.set_logging_level("INFO")
 
+def plot_experimental_data(axes, Crate, temperature, cells_ignore):
+    dataset = import_thermal_data(Crate, temperature)
+    data_conc = {"time": [], "voltage": [], "temperature": []}
+
+    for cell, data in dataset.items():
+        if cell in cells_ignore:
+            continue
+
+        idx_start, idx_end = get_idxs(data, Crate * 5, 5 / 3)
+        if len(idx_end) == 1:
+            idx_end = np.append(idx_end, len(data["Time [s]"]))
+        axes[0].plot(
+            data["Time [s]"][idx_start[0] : idx_end[1]]
+            - data["Time [s]"][idx_start[0]],
+            data["Voltage [V]"][idx_start[0] : idx_end[1]],
+            label=cell,
+            linewidth=1,
+            ls="--",
+        )
+        axes[1].plot(
+            data["Time [s]"][idx_start[0] : idx_end[1]]
+            - data["Time [s]"][idx_start[0]],
+            data["Temp Cell [degC]"][idx_start[0] : idx_end[1]],
+            label=cell,
+            linewidth=1,
+            ls="--",
+        )
+        pad = 4
+
+        axes[0].annotate(
+            "{}C".format(Crate),
+            xy=(0, 0.5),
+            xytext=(-axes[0].yaxis.labelpad - pad, 0),
+            xycoords=axes[0].yaxis.label,
+            textcoords="offset points",
+            size="large",
+            ha="right",
+            va="center",
+        )
+
+        data_conc["time"] = np.append(
+            data_conc["time"],
+            data["Time [s]"][idx_start[0] : idx_end[1]]
+            - data["Time [s]"][idx_start[0]],
+        )
+        data_conc["voltage"] = np.append(
+            data_conc["voltage"], data["Voltage [V]"][idx_start[0] : idx_end[1]]
+        )
+        data_conc["temperature"] = np.append(
+            data_conc["temperature"],
+            data["Temp Cell [degC]"][idx_start[0] : idx_end[1]],
+        )
+
+    return axes, data_conc
+
 def plot_model_solutions(axes, solution, Crate, temperature):
-    if solution.model.name == "TSPMe":
+    if solution.all_models[0].name == "TDFN":
         ls = "-"
         color = "black"
         linewidth = 0.75
@@ -34,12 +89,12 @@ def plot_model_solutions(axes, solution, Crate, temperature):
         solution["Time [s]"].entries,
         solution["Terminal voltage [V]"].entries,
         color=color,
-        label=solution.model.name,
+        label=solution.all_models[0].name,
         ls=ls,
         linewidth=linewidth,
     )
 
-    if solution.model.name == "TSPMe":
+    if solution.all_models[0].name == "TDFN":
         axes[0].scatter(
             0,
             solution["X-averaged battery open circuit voltage [V]"].entries[0],
@@ -56,7 +111,7 @@ def plot_model_solutions(axes, solution, Crate, temperature):
         solution["Time [s]"].entries,
         solution["X-averaged cell temperature [K]"].entries - 273.15,
         color=color,
-        label=solution.model.name,
+        label=solution.all_models[0].name,
         ls=ls,
         linewidth=linewidth,
     )
@@ -70,8 +125,8 @@ def plot_model_solutions(axes, solution, Crate, temperature):
 
     return axes
 
-def compare_data(models,param,Crate,temperature):
-    fig,axes=plt.subplots(3,2,figsize=(5.7,5.5))
+def compare_data(models,param,Crate,temperature,cells_ignore=None):
+    fig,axes=plt.subplots(3,2,figsize=(10,15))
     for k,Crate in enumerate(Crates):
         param = set_experiment_parameters(param, Crate, temperature)
         param = set_ambient_temperature(param, Crate, temperature)
@@ -81,6 +136,11 @@ def compare_data(models,param,Crate,temperature):
             
         )
         solutions=[]
+
+        axes[k,:],data_conc=plot_experimental_data(
+            axes[k,:],Crate,temperature,cells_ignore
+        )
+
         for model in models:
             simulation=pybamm.Simulation(
                 model,
@@ -89,6 +149,12 @@ def compare_data(models,param,Crate,temperature):
             )
             solution=simulation.solve()
             solutions.append(solution)
+            axes[k,:]=plot_model_solutions(axes[k,:],solution,Crate,temperature)
+        
+        fig.suptitle(f"Ambient temperature: {temperature} °C")
+        fig.tight_layout()
+        fig.subplots_adjust(left=0.15, top=0.92)
+    return fig
 
 models=[
     pybamm.lithium_ion.DFN(
@@ -96,29 +162,26 @@ models=[
             "thermal":"lumped",
             "cell geometry": "arbitrary",
         },
-        name="TDFN"
+        name="TDFN_arbitrary"
     ),
-    # pybamm.lithium_ion.SPMe(
-    #     options={
-    #         "thermal":"lumped",
-    #         "cell geometry": "arbitrary",
-    #         "electrolyte conductivity":"integrated",
-    #     },
-    #     name="TSPMe"
-    # )
-    pybamm.lithium_ion.DFN(
+    pybamm.lithium_ion.SPMe(
         options={
-            "thermal":"lumped"
+            "thermal":"lumped",
         },
-        name="TSPMe"
+        name="TDFN"
     )
 ]
 
 param=pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Chen2020)
 temperatures=[0,10,25]
 Crates=[0.5,1,2]
+cells_ignore = ["791"]
 root=os.path.dirname(os.path.dirname(__file__))
 
 for temperature in temperatures:
     param=set_thermal_parameters(param,16,2.32e6,temperature)
-    compare_data(models,param,Crates,temperature)
+    fig=compare_data(models,param,Crates,temperature,cells_ignore=cells_ignore)
+    # plt.show()
+    fig.savefig(
+        os.path.join(root, "figures", "mytest2_{}degC.png".format(temperature)),
+    )
